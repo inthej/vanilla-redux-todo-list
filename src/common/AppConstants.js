2f1510a25bc4994c2db8e68bd9502fdc4af08a84
inthej <inthej12@gmail.com>
2022-02-02 20:26:53 +0900

    Update Todo Process

---
+++ src/common/AppConstants.js
@@ -0,0 +1,6 @@
+const AppConstants = {}
+
+// Document
+AppConstants.ENTER_KEY_CODE = 13
+
+export default AppConstants
--- src/index.js
+++ src/index.js
@@ -1,1 +1,115 @@
-console.log('hello world')
+import { createStore } from 'redux'
+import $ from 'jquery'
+import AppConstants from "./common/AppConstants";
+
+// DOM Reference
+const myInput = $('#myInput')
+const addBtn = $('.addBtn')
+const todoList = $('#todoList')
+
+// Action Types
+const ADD_TODO = 'ACC_TODO'
+const CLOSE_TODO = 'CLOSE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+
+// Action Create Function
+const addTodo = text => ({ type: ADD_TODO, text })
+const closeTodo = todoIdx => ({ type: CLOSE_TODO, todoIdx })
+const toggleTodo = todoIdx => ({ type: TOGGLE_TODO, todoIdx })
+
+// InitialState
+const initialState = {
+  todos: [], // { idx:0, checked: false, text: 'text' }
+  todoIndexRef: 0
+}
+
+// Reducer
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      state = {...state, todoIndexRef: state.todoIndexRef + 1}
+      const todo = {
+        idx: state.todoIndexRef,
+        checked: false,
+        text: action.text,
+      }
+      return {
+        ...state,
+        todos: state.todos.concat(todo)
+      }
+    case CLOSE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.idx !== action.todoIdx)
+      }
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map(todo => todo.idx === action.todoIdx ? {
+          ...todo,
+          checked: !todo.checked
+        } : todo)
+      }
+    default:
+      return state
+  }
+}
+
+// store
+const store = createStore(reducer)
+
+// render
+const render = () => {
+  const state = store.getState()
+
+  todoList.empty()
+
+  state.todos.forEach(todo => {
+    const itemId = `item-${todo.idx}`
+    const closeBtnId = `close-${todo.idx}`
+    const todoElementLi = `<li id="${itemId}" ${todo.checked ? 'class="checked"' : ''} >${todo.text}<span class="close" id="${closeBtnId}">x</span></li>`;
+    todoList.append(todoElementLi)
+
+    // toggle event
+    $(`#${itemId}`).on('click', function () {
+      store.dispatch(toggleTodo(todo.idx))
+    })
+
+    // close event
+    $(`#${closeBtnId}`).on('click', function () {
+      store.dispatch(closeTodo(todo.idx))
+    })
+  })
+}
+
+render()
+
+// subscribe
+store.subscribe(render)
+
+// dispatch
+addBtn.on('click', function () {
+  dispatchAddTodo()
+})
+
+// dispatch
+myInput.on("keyup", function (key) {
+  if (key.keyCode === AppConstants.ENTER_KEY_CODE) {
+    dispatchAddTodo()
+  }
+});
+
+const dispatchAddTodo = () => {
+  const inputText = myInput.val()
+  const isEmptyInput = inputText.length === 0;
+  if (isEmptyInput) {
+    return
+  }
+
+  store.dispatch(addTodo(inputText))
+  resetInput()
+}
+
+const resetInput = () => {
+  myInput.val('')
+}
